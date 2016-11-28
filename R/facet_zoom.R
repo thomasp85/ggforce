thomasp85/ggforce

commit e2d3ab7cc24d7d56da97abb5d0590abd9d07e048

Fix #21

R/facet_zoom.R
@@ -97,11 +97,15 @@ FacetZoom <- ggproto("FacetDuplicate", Facet,
             cbind(data, PANEL = 1L),
             if ('x' %in% layout$name) {
                 index_x <- tryCatch(lazy_eval(params$x, data), error = function(e) FALSE)
-                cbind(data[index_x, ], PANEL = layout$PANEL[layout$name == "x"])
+                if (sum(index_x) != 0) {
+                    cbind(data[index_x, ], PANEL = layout$PANEL[layout$name == "x"])
+                }
             },
             if ('y' %in% layout$name) {
                 index_y <- tryCatch(lazy_eval(params$y, data), error = function(e) FALSE)
-                cbind(data[index_y, ], PANEL = layout$PANEL[layout$name == "y"])
+                if (sum(index_y) != 0) {
+                    cbind(data[index_y, ], PANEL = layout$PANEL[layout$name == "y"])
+                }
             }
         )
     },

commit 9270db7f38fc99bb0115736144c4b31ccfde5d6a

Correct error with NA values
Bug introduced in e2d3ab7

R/facet_zoom.R
@@ -97,13 +97,13 @@ FacetZoom <- ggproto("FacetDuplicate", Facet,
             cbind(data, PANEL = 1L),
             if ('x' %in% layout$name) {
                 index_x <- tryCatch(lazy_eval(params$x, data), error = function(e) FALSE)
-                if (sum(index_x) != 0) {
+                if (sum(index_x, na.rm = TRUE) != 0) {
                     cbind(data[index_x, ], PANEL = layout$PANEL[layout$name == "x"])
                 }
             },
             if ('y' %in% layout$name) {
                 index_y <- tryCatch(lazy_eval(params$y, data), error = function(e) FALSE)
-                if (sum(index_y) != 0) {
+                if (sum(index_y, na.rm = TRUE) != 0) {
                     cbind(data[index_y, ], PANEL = layout$PANEL[layout$name == "y"])
                 }
             }