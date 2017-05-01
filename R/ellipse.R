31cfa2d66107b52e365e792676857d0f3a955c8e
Thomas Lin Pedersen <thomasp85@gmail.com>
2017-05-01 19:24:02 +0200

    Add docs

--- R/ellipse.R
+++ R/ellipse.R
@@ -1,11 +1,60 @@
+#' Draw ellipses based on the coordinate system scale
+#'
+#' This is a generalisation of \code{\link{geom_circle}} that allows you to draw
+#' ellipses at a specified angle and center relative to the coordinate system.
+#'
+#' @section Aesthetics:
+#' geom_arc understand the following aesthetics (required aesthetics are in
+#' bold):
+#' \itemize{
+#'  \item{\strong{x0}}
+#'  \item{\strong{y0}}
+#'  \item{\strong{a}}
+#'  \item{\strong{b}}
+#'  \item{\strong{angle}}
+#'  \item{color}
+#'  \item{fill}
+#'  \item{size}
+#'  \item{linetype}
+#'  \item{alpha}
+#'  \item{lineend}
+#' }
+#'
+#' @section Computed variables:
+#'
+#' \describe{
+#'  \item{x, y}{The coordinates for the points along the ellipse}
+#' }
+#'
+#' @inheritParams ggplot2::geom_path
+#'
+#' @param n The number of points to sample along the ellipse.
+#'
+#' @author Thomas Lin Pedersen
+#'
+#' @name geom_ellipse
+#' @rdname geom_ellipse
+#'
+#' @examples
+#' # Basic usage
+#' ggplot() +
+#'   geom_ellipse(aes(x0 = 0, y0 = 0, a = 10, b = 3, angle = 0)) +
+#'   coord_fixed()
+#'
+#' # Rotation
+#' # Note that it expects radians and rotates the ellipse counter-clockwise
+#' ggplot() +
+#'   geom_ellipse(aes(x0 = 0, y0 = 0, a = 10, b = 3, angle = pi/4)) +
+#'   coord_fixed()
+#'
+NULL
 
 #' @rdname ggforce-extensions
 #' @format NULL
 #' @usage NULL
 #' @importFrom ggplot2 ggproto Stat
-#' @importFrom grid arcCurvature
 #' @export
-StatCircle <- ggproto('StatCircle', Stat,
+StatEllipse<- ggproto('StatEllipse', Stat,
     compute_layer = function(self, data, params, layout) {
         n_ellipses <- nrow(data)
         data <- data[rep(seq_len(n_ellipses), each = params$n), ]
@@ -21,7 +70,7 @@ StatCircle <- ggproto('StatCircle', Stat,
 #' @rdname geom_circle
 #' @importFrom ggplot2 layer
 #' @export
-stat_circle  <- function(mapping = NULL, data = NULL, geom = "circle",
+stat_ellipse  <- function(mapping = NULL, data = NULL, geom = "circle",
                          position = "identity", n = 360, na.rm = FALSE, show.legend = NA,
                          inherit.aes = TRUE, ...) {
     layer(
@@ -30,18 +79,11 @@ stat_circle  <- function(mapping = NULL, data = NULL, geom = "circle",
         params = list(na.rm = na.rm, n = n, ...)
     )
 }
-#' @rdname ggforce-extensions
-#' @format NULL
-#' @usage NULL
-#' @importFrom ggplot2 ggproto GeomPolygon
-#' @export
-GeomCircle <- ggproto('GeomCircle', GeomPolygon,
-                      default_aes = list(colour = 'black', fill = NA, size = 0.5, linetype = 1, alpha = NA)
-)
+
 #' @rdname geom_circle
 #' @importFrom ggplot2 layer
 #' @export
-geom_circle <- function(mapping = NULL, data = NULL, stat = "circle",
+geom_ellipse <- function(mapping = NULL, data = NULL, stat = "ellipse",
                         position = "identity", n = 360, na.rm = FALSE,
                         show.legend = NA, inherit.aes = TRUE, ...) {
     layer(data = data, mapping = mapping, stat = stat, geom = GeomCircle,
