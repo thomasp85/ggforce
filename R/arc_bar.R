c710748375816682bf4e8bf9263cb6b314aee50a
Thomas Lin Pedersen <thomasp85@gmail.com>
2019-02-27 22:19:30 +0100

    Corrrectly pass shape parameters on

--- R/arc_bar.R
+++ R/arc_bar.R
@@ -187,7 +187,7 @@ geom_arc_bar <- function(mapping = NULL, data = NULL, stat = 'arc_bar',
   layer(
     data = data, mapping = mapping, stat = stat, geom = GeomArcBar,
     position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-    params = list(na.rm = na.rm, n = n, ...)
+    params = list(na.rm = na.rm, n = n, expand = expand, radius = radius, ...)
   )
 }
 
