6bc8e67023b969c221bea31f92f72df051b246cc
Thomas Lin Pedersen <thomasp85@gmail.com>
2019-03-01 09:20:09 +0100

    Update mark examples for pkgdown

--- R/mark_rect.R
+++ R/mark_rect.R
@@ -41,27 +41,28 @@
 #'
 #' # Long descriptions are automatically wrapped to fit into the width
 #' iris$desc <- c(
-#'   'A super Iris — and it knows it',
+#'   'A super Iris - and it knows it',
 #'   'Pretty mediocre Iris, but give it a couple of years and it might surprise you',
 #'   "You'll never guess what this Iris does every Sunday"
 #' )[iris$Species]
 #'
 #' ggplot(iris, aes(Petal.Length, Petal.Width)) +
-#'   geom_mark_rect(aes(fill = Species, label = Species, description = desc)) +
+#'   geom_mark_rect(aes(fill = Species, label = Species, description = desc,
+#'                      filter = Species == 'setosa')) +
 #'   geom_point()
 #'
 #' # Change the buffer size to move labels farther away (or closer) from the
 #' # marks
 #' ggplot(iris, aes(Petal.Length, Petal.Width)) +
-#'   geom_mark_rect(aes(fill = Species, label = Species, description = desc),
-#'                    label.buffer = unit(40, 'mm')) +
+#'   geom_mark_rect(aes(fill = Species, label = Species),
+#'                  label.buffer = unit(40, 'mm')) +
 #'   geom_point()
 #'
 #' # The connector is capped a bit before it reaches the mark, but this can be
 #' # controlled
 #' ggplot(iris, aes(Petal.Length, Petal.Width)) +
-#'   geom_mark_rect(aes(fill = Species, label = Species, description = desc),
-#'                    con.cap = 0) +
+#'   geom_mark_rect(aes(fill = Species, label = Species),
+#'                  con.cap = 0) +
 #'   geom_point()
 NULL
 
