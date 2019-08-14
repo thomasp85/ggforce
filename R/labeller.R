ef0d73952f8c65d79f6c68ae6dfc2e78925095c1
atusy <30277794+atusy@users.noreply.github.com>
2019-08-14 10:28:01 +0900

    add: label_tex

---
+++ R/labeller.R
@@ -0,0 +1,26 @@
+#' A labeller function to parse TeX syntax
+#'
+#' Thi function formats the strip labels of facet grids and wraps that contains
+#' TeX expressions.
+#'
+#' @seealso [ggplot2::labeller], [latex2exp::TeX()]
+#'
+#' @inheritParams ggplot2::label_parsed
+#' @inheritDotParams ggplot2::label_parsed -labels
+#'
+#' @examples
+#' library(ggplot2)
+#' d <- data.frame(x = 1, y = 1, facet = "$\\beta$")
+#' ggplot(d, aes(x, y)) +
+#'  geom_point() +
+#'  facet_wrap(~ facet, labeller = label_tex)
+#'
+#' @importFrom latex2exp TeX
+#' @importFrom ggplot2 label_parsed
+#' @export
+label_tex <- function(labels, ...) {
+  label_parsed(
+    as.data.frame(lapply(labels, TeX, output = "character")),
+    ...
+  )
+}
