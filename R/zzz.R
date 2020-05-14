360764905001063ce550e4e6e3efa8b1223a9bdc
teunbrand <tahvdbrand@gmail.com>
2020-05-14 20:01:59 +0200

    Register zoom theme elements on load.

--- R/zzz.R
+++ R/zzz.R
@@ -5,4 +5,27 @@ default_axis_guide <- NULL
   } else {
     default_axis_guide <<- "none"
   }
+  
+  # Registering of theme elements
+  register_theme_elements(
+    ggforce.zoom = element_rect(),
+    ggforce.zoom.funnel = element_rect(),
+    ggforce.zoom.funnel.x = element_rect(),
+    ggforce.zoom.funnel.y = element_rect(),
+    ggforce.zoom.area = element_rect(),
+    ggforce.zoom.area.x = element_rect(),
+    ggforce.zoom.area.y = element_rect(),
+    element_tree = list(
+      ggforce.zoom = el_def("element_rect", "strip.background", 
+                            "zoom elements: funnels and areas"),
+      ggforce.zoom.funnel = el_def("element_rect", "ggforce.zoom",
+                                   "zoom funnel between panels"),
+      ggforce.zoom.funnel.x = el_def("element_rect", "ggforce.zoom.funnel"),
+      ggforce.zoom.funnel.y = el_def("element_rect", "ggforce.zoom.funnel"),
+      ggforce.zoom.area = el_def("element_rect", "ggforce.zoom",
+                                 "indicator area for zooming"),
+      ggforce.zoom.area.x = el_def("element_rect", "ggforce.zoom.area"),
+      ggforce.zoom.area.y = el_def("element_rect", "ggforce.zoom.area")
+    )
+  )
 }
