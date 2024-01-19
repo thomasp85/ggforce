f551f33f29d115df2beb1c093c3e6b7387bd3ed8
Thomas Lin Pedersen <thomasp85@gmail.com>
2024-01-19 08:57:24 +0100

    make sure concaveman always returns the same format of data

--- R/concaveman.R
+++ R/concaveman.R
@@ -1,5 +1,5 @@
 concaveman <- function(points, concavity, threshold) {
-  if (nrow(points) < 4) return(points)
+  if (nrow(points) < 4) return(unname(points))
   hull <- as.integer(grDevices::chull(points)) - 1L
   concaveman_c(points, hull, concavity, threshold)
 }
