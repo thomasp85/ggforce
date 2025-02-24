2c3336bb690d1d5b88c5245ae24b742dbd86845f
Teun van den Brand <tahvdbrand@gmail.com>
2025-02-24 20:26:32 +0100

    bump aesthetics upon load

--- R/zzz.R
+++ R/zzz.R
@@ -5,6 +5,17 @@ default_axis_guide <- NULL
   } else {
     default_axis_guide <<- "none"
   }
+
+  if ("element_geom" %in% getNamespaceExports("ggplot2")) {
+    ggplot2::update_geom_defaults(GeomArc0, ggplot2::aes(colour = from_theme(ink), linewidth = from_theme(linewidth), linetype = from_theme(linetype)))
+    ggplot2::update_geom_defaults(GeomArcBar, ggplot2::aes(colour = from_theme(ink)))
+    ggplot2::update_geom_defaults(GeomCircle, ggplot2::aes(colour = from_theme(ink)))
+    ggplot2::update_geom_defaults(GeomMarkCircle, ggplot2::aes(colour = from_theme(ink)))
+    ggplot2::update_geom_defaults(GeomMarkEllipse, ggplot2::aes(colour = from_theme(ink)))
+    ggplot2::update_geom_defaults(GeomMarkHull, ggplot2::aes(colour = from_theme(ink)))
+    ggplot2::update_geom_defaults(GeomMarkRect, ggplot2::aes(colour = from_theme(ink)))
+  }
+
   ggplot2::register_theme_elements(
     zoom = element_rect(),
     zoom.x = element_rect(),
