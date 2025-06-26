4ee4f47926e6d7aa524ad71496b0d94bf377bfc4
Alexey Sergushichev <alsergbox@gmail.com>
2025-06-26 09:48:30 -0500

    geom_mark_shape docs

--- R/mark_shape.R
+++ R/mark_shape.R
@@ -1,18 +1,15 @@
-#' Annotate areas with hulls
+#' Annotate areas with polygonal shapes
 #'
-#' This geom lets you annotate sets of points via hulls. While convex hulls are
-#' most common due to their clear definition, they can lead to large areas
-#' covered that does not contain points. Due to this `geom_mark_hull` uses
-#' concaveman which lets you adjust concavity of the resulting hull. The hull is
-#' calculated at draw time, and can thus change as you resize the plot. In order
-#' to clearly contain all points, and for aesthetic purpose the resulting hull
-#' is expanded 5mm and rounded on the corners. This can be adjusted with the
-#' `expand` and `radius` parameters.
+#' This geom lets you annotate sets of points via polygonal shapes. 
+#' Unlike other `geom_mark_*` functions, `geom_mark_shape` should be explicitly
+#' provided with the shape coordinates. As in `geom_shape`, the polygon can be
+#' expanded/contracted and corners can be rounded, which is controlled by `expand` and
+#' `radius` parameters.
 #'
 #' @inheritSection geom_mark_circle Annotation
 #' @inheritSection geom_mark_circle Filtering
 #' @section Aesthetics:
-#' `geom_mark_hull` understand the following aesthetics (required aesthetics are
+#' `geom_mark_shape` understand the following aesthetics (required aesthetics are
 #' in bold):
 #'
 #' - **x**
@@ -31,68 +28,24 @@
 #'
 #' @inheritParams geom_mark_circle
 #'
-#' @param concavity A measure of the concavity of the hull. `1` is very concave
-#' while it approaches convex as it grows. Defaults to `2`.
 #'
 #' @family mark geoms
 #' @name geom_mark_shape
 #' @rdname geom_mark_shape
 #'
 #' @examples
-#' ggplot(iris, aes(Petal.Length, Petal.Width)) +
-#'   geom_mark_hull(aes(fill = Species, filter = Species != 'versicolor')) +
-#'   geom_point()
-#'
-#' # Adjusting the concavity lets you change the shape of the hull
-#' ggplot(iris, aes(Petal.Length, Petal.Width)) +
-#'   geom_mark_hull(aes(fill = Species, filter = Species != 'versicolor'),
-#'     concavity = 1
-#'   ) +
-#'   geom_point()
-#'
-#' ggplot(iris, aes(Petal.Length, Petal.Width)) +
-#'   geom_mark_hull(aes(fill = Species, filter = Species != 'versicolor'),
-#'     concavity = 10
-#'   ) +
-#'   geom_point()
-#'
-#' # Add annotation
-#' ggplot(iris, aes(Petal.Length, Petal.Width)) +
-#'   geom_mark_hull(aes(fill = Species, label = Species)) +
-#'   geom_point()
-#'
-#' # Long descriptions are automatically wrapped to fit into the width
-#' iris$desc <- c(
-#'   'A super Iris - and it knows it',
-#'   'Pretty mediocre Iris, but give it a couple of years and it might surprise you',
-#'   "You'll never guess what this Iris does every Sunday"
-#' )[iris$Species]
-#'
-#' ggplot(iris, aes(Petal.Length, Petal.Width)) +
-#'   geom_mark_hull(aes(fill = Species, label = Species, description = desc,
-#'                      filter = Species == 'setosa')) +
-#'   geom_point()
-#'
-#' # Change the buffer size to move labels farther away (or closer) from the
-#' # marks
-#' ggplot(iris, aes(Petal.Length, Petal.Width)) +
-#'   geom_mark_hull(aes(fill = Species, label = Species),
-#'                  label.buffer = unit(40, 'mm')) +
-#'   geom_point()
-#'
-#' # The connector is capped a bit before it reaches the mark, but this can be
-#' # controlled
-#' ggplot(iris, aes(Petal.Length, Petal.Width)) +
-#'   geom_mark_hull(aes(fill = Species, label = Species),
-#'                  con.cap = 0) +
-#'   geom_point()
-#'
-#' # If you want to use the scaled colours for the labels or connectors you can
-#' # use the "inherit" keyword instead
-#' ggplot(iris, aes(Petal.Length, Petal.Width)) +
-#'   geom_mark_hull(aes(fill = Species, label = Species),
-#'                  label.fill = "inherit") +
-#'   geom_point()
+#' set.seed(42)
+#' points <- data.frame(
+#'     x=runif(100, min=0, max=3),
+#'     y=runif(100, min=0, max=3))
+#' shape <- data.frame(
+#'     x = c(0, 3, 3, 2, 2, 1, 1, 0),
+#'     y = c(0, 0, 3, 3, 1, 1, 3, 3)
+#' )
+#' 
+#' ggplot(points, aes(x=x, y=y)) +
+#'     geom_point() +
+#'     geom_mark_shape(data=shape, color="red")
 #'
 NULL
 
